fb7ed4138a00cc12b980fc983b2306f10a0b5617
samciu <samciu@163.com>
2015-08-15 21:42:06 +0800

    基本完成课程模块

--- js/index.js
+++ js/index.js
@@ -198,25 +198,31 @@ function page(opt){
     var allNum = opt.allNum || childLength;
     var callback = opt.callback || function(){};
     // 可显示页数二分之一+1的位置
-    var point = Math.floor(childLength/2) + 1;    
-    
-    var prvPage = document.querySelector('.m-pager .prv');
-    var pageNum = document.querySelectorAll('.m-pager .pg');
-    var nxtPage = document.querySelector('.m-pager .nxt');
+    var point = Math.floor(childLength/2) + 1;
     
     //当前页不等于1时上一页可选
+    var oA = document.createElement('a');    
+    oA.innerText = '上一页';
+    oA.setAttribute('index',nowNum - 1);
     if(nowNum != 1){
-        prvPage.setAttribute('index',nowNum - 1);
-        removeClass(prvPage,'f-dis');
+        oA.className = 'prv';
     }
     else{
-        addClass(prvPage,'f-dis');
-    }
+        oA.className = 'prv f-dis';
+    }    
+    obj.appendChild(oA);
+    
     //生成具体页数，总页数小于等于可显示页数的情况
     if(allNum <= childLength){
-        for(var i=1; i <= allNum; i++){            
-            pageNum[i-1].setAttribute('index',i);
-            pageNum[i-1].innerText = i;
+        for(var i=1; i <= allNum; i++){ 
+            var oA = document.createElement('a');
+            oA.setAttribute('index',i);
+            oA.className = 'pg';
+            oA.innerText = i;
+            if(nowNum == i){
+                addClass(oA,'selected');
+            }
+            obj.appendChild(oA);
         }
     }
     //生成具体页数，总页数大于可显示页数的情况
@@ -224,43 +230,65 @@ function page(opt){
         for(var i=1; i <= childLength; i++){
             //当前页是小于一半+1的可显示页数
             if(nowNum < point){
-                pageNum[i-1].setAttribute('index',i);
-                pageNum[i-1].innerText = i;
+                var oA = document.createElement('a');
+                oA.setAttribute('index',i);
+                oA.className = 'pg';
+                oA.innerText = i;
+                if(nowNum == i){
+                    addClass(oA,'selected');
+                }
             }
             //当前页是倒数第1或倒数第2
             else if(allNum - nowNum <= point){
-                pageNum[i-1].setAttribute('index',allNum - childLength +i);
-                pageNum[i-1].innerText = allNum - childLength +i;
+                var oA = document.createElement('a');
+                oA.setAttribute('index',allNum - childLength +i);
+                oA.className = 'pg';
+                oA.innerText = allNum - childLength +i;
+                if(nowNum == allNum - childLength +i){
+                    addClass(oA,'selected');
+                }
             }
             //当前页在可显示页数一半+1的位置显示，例如可以显示8页，当前页就在第5个位置
             else{
-                pageNum[i-1].setAttribute('index',nowNum - point + i);
-                pageNum[i-1].innerText = nowNum - point + i;
-            }
+                var oA = document.createElement('a');
+                oA.setAttribute('index',nowNum - point + i);
+                oA.className = 'pg';
+                oA.innerText = nowNum - point + i;
+                if(i == point){
+                    addClass(oA,'selected');
+                }
+            }            
+            obj.appendChild(oA);
         }
     }
     //当前页不是最后一页时显示下一页
+    var oA = document.createElement('a');    
+    oA.innerText = '下一页';    
+    oA.setAttribute('index',nowNum + 1);
     if(allNum != nowNum){
-        nxtPage.setAttribute('index',nowNum + 1);
-        removeClass(nxtPage,'f-dis');
+        oA.className = 'nxt';
     }
     else{
-        addClass(nxtPage,'f-dis');        
+        oA.className = 'nxt f-dis';
     }
+    obj.appendChild(oA);
     
     //用addevent会重复注册
     var aA = obj.getElementsByTagName('a');
     for(var i=0;i<aA.length;i++){
         aA[i].onclick=function(){
-            var nowNum = parseInt(this.getAttribute('index'));
-            page({
-                id:opt.id,
-                nowNum:nowNum,
-                allNum:allNum,
-                childLength:childLength,
-                callback:callback
-            });
-            callback(nowNum,allNum);
+            if(nowNum != parseInt(this.getAttribute('index'))){
+                var nowNum = parseInt(this.getAttribute('index'));
+                obj.innerHTML = '';
+                page({
+                    id:opt.id,
+                    nowNum:nowNum,
+                    allNum:allNum,
+                    childLength:childLength,
+                    callback:callback
+                });
+                callback(nowNum,allNum);
+            }            
             return false;
         }
     }
@@ -272,7 +300,7 @@ function page(opt){
 /*---------------------------------应用函数开始--------------------------------------------*/    
 
 var url = "http://study.163.com/webDev/couresByCategory.htm";
-var pageSize = 10;
+var pageSize = 8;
 var pageType = 10;
 
 var mnav = document.querySelector('.m-nav');
@@ -287,6 +315,7 @@ delegateEvent(mnav,'a','click',
             }
             addClass(target,'selected');
             pageType = target.getAttribute('data');
+            mpager.innerHTML = '';
             getPageNum(1);
         }
         event.preventDefault();
@@ -311,13 +340,39 @@ function initPager(response,now){
     page(option);
 }
 //获取课程列表
-function getCourse(now,all){        
+function getCourse(now,all){
+    console.log(now);
+    
     var options = {pageNo:now,psize:pageSize,type:pageType};
     get(url,options,drawCourse);
 }
 //生成课程列表
 function drawCourse(response){
     var data = JSON.parse(response);
+    console.log(data.pagination.pageIndex);
+    
+    var boo = document.querySelectorAll('.u-cover');
+    for(var i=boo.length-1;i>0;i--){
+        boo[i].parentNode.removeChild(boo[i]);
+    }
+    
+    var templete = document.querySelector('.m-data-lists .f-templete');
+        
+    for(var i=0,list=data.list;i<list.length;i++){       
+        var cloned = templete.cloneNode(true);
+        removeClass(cloned,'f-templete');
+        var imgpic = cloned.querySelector('.imgpic');
+        var title = cloned.querySelector('.tt');
+        var orgname = cloned.querySelector('.orgname');
+        var pri = cloned.querySelector('.pri');
+        
+        imgpic.src = list[i].middlePhotoUrl;
+        imgpic.alt = list[i].name;
+        title.innerText = list[i].name;
+        orgname.innerText = list[i].provider;
+        pri.innerText = '￥' + list[i].price + '.00';        
+        templete.parentNode.appendChild(cloned);
+    }
 }
 
 getPageNum(1);    
